cbe2da5b8a06e53837d167fc562b0ee1eb0210e9
Mujtaba-aamir <ma7820464@gmail.com>
2026-06-21 04:16:40 +0500

    Added user login method

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -13,7 +13,20 @@ class AuthenticationController extends Controller
         return view('auth.login');
     }
 
-    public function loginUser(Request $req) {}
+    public function loginUser(Request $req)
+    {
+        $user = $req->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        if (Auth::attempt($user)) {
+            return redirect()->route("dashboardPage");
+        }
+
+        else
+            return back()->withErrors(["message" => "Invalid email or password"]);
+    }
 
     public function showRegisterForm()
     {
@@ -24,8 +37,15 @@ class AuthenticationController extends Controller
     {
         $user = $req->validate([
             "name" => 'required|max:30|string',
-            "email" => 'required|unique:users,email',
+            "email" => 'required|email|unique:users,email',
             "password" => 'required|confirmed|min:8'
         ]);
+        $Register = User::create($user);
+        return redirect()->back()->with('success', 'Registered Successfully, Please Log in');
+    }
+
+    public function showDashboardPage()
+    {
+        return view('admin.dashboard');
     }
 }
